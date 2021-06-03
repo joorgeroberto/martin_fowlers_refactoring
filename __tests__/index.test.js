77215cfe696351a04c0ee13492f3a9ac0dbd65a1
joorgeroberto <jorgercj@dcomp.ufs.br>
2021-06-02 22:51:35 -0300

    Change mock name

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,10 +5,10 @@ const invoices = require("../__mocks__/utils/invoices.json");
 const plays = require("../__mocks__/utils/plays.json");
 
 describe("Statement function tests", () => {
-  const BigCo = invoices.find(({ customer }) => customer === "BigCo");
-  test(`should be able to pass in BigCo test`, () => {
-    expect(statement(BigCo, plays)).toBe(
-      `Statement for BigCo\n  Hamlet: $650.00 (55 seats)\n  As You Like It: $580.00 (35 seats)\n  Othello: $500.00 (40 seats)\nAmount owed is $1,730.00\nYou earned 47 credits\n`
+  const BigCoI = invoices.find(({ customer }) => customer === "BigCo I");
+  test(`should be able to pass in BigCo I test`, () => {
+    expect(statement(BigCoI, plays)).toBe(
+      `Statement for BigCo I\n  Hamlet: $650.00 (55 seats)\n  As You Like It: $580.00 (35 seats)\n  Othello: $500.00 (40 seats)\nAmount owed is $1,730.00\nYou earned 47 credits\n`
     );
   });
 
